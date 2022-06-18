8b3b51f42efcc89b1113ae2bf36f32b8abf726a5
Ellen Spampinato <flerzaytranj@gmail.com>
2022-06-17 20:59:12 -0400

    first commit

---
+++ tutorials/tests.py
@@ -0,0 +1,60 @@
+from django.test import TestCase
+from django.urls import reverse
+import pytest
+
+#for integration testing
+from tutorials.models import Tutorial
+
+# Create your tests here.
+
+#unit test--verifies home url
+def test_homepage_access():
+    url = reverse('home')
+    assert url == "/"
+
+#integration test
+""" 
+@pytest.mark.django_db
+def test_create_tutorial():
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    assert tutorial.title == "Pytest"
+ """
+#integration test with a fixture
+@pytest.fixture
+def new_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+#more integration tests
+def test_search_tutorials(new_tutorial):
+    assert Tutorial.objects.filter(title='Pytest').exists()
+
+def test_update_tutorial(new_tutorial):
+    new_tutorial.title = 'Pytest-Django'
+    new_tutorial.save()
+    assert Tutorial.objects.filter(title='Pytest-Django').exists()
+
+#more fixture functions
+@pytest.fixture
+def another_tutorial(db):
+    tutorial = Tutorial.objects.create(
+        title='More-Pytest',
+        tutorial_url='https://pytest-django.readthedocs.io/en/latest/index.html',
+        description='Tutorial on how to apply pytest to a Django application',
+        published=True
+    )
+    return tutorial
+
+def test_compare_tutorials(new_tutorial, another_tutorial):
+    assert new_tutorial.pk != another_tutorial.pk
+
